47ad0d00e4be282c20424cf23577bed1b53444e2
shad7 <kenny.shad7@gmail.com>
2015-01-09 00:51:34 -0500

    added oslo_config.fixture to base test class.

--- tvrenamer/tests/base.py
+++ tvrenamer/tests/base.py
@@ -1,5 +1,13 @@
+from oslo_config import fixture as fixture_config
 from oslotest import base as test_base
+
+import tvrenamer
 
 
 class BaseTest(test_base.BaseTestCase):
-    pass
+
+    def setUp(self):
+        super(BaseTest, self).setUp()
+
+        self.CONF = self.useFixture(fixture_config.Config()).conf
+        self.CONF([], project=tvrenamer.PROJECT_NAME)
