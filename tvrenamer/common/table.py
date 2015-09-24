49f76fea2b593570b8bda8ca46f40ffda1d337d2
shad <kenny.shad7@gmail.com>
2015-09-23 23:33:13 -0400

    [FEATURE] Enable trakt as data service
    Add data service plugin for trakt.tv

--- tvrenamer/common/table.py
+++ tvrenamer/common/table.py
@@ -29,21 +29,20 @@ def write_output(data):
     # first row and set the alignment of the
     # output accordingly.
     data_iter = iter(data)
-    try:
-        first_row = next(data_iter)
-    except StopIteration:
-        pass
-    else:
-        for value, name in zip(first_row, COLUMNS):
-            alignment = ALIGNMENTS.get(type(value), 'l')
-            x.align[name] = alignment
-        # Now iterate over the data and add the rows.
-        x.add_row(first_row)
-        for row in data_iter:
-            row = [r.replace('\r\n', '\n').replace('\r', ' ')
-                   if isinstance(r, six.string_types) else r
-                   for r in row]
-            x.add_row(row)
+    first_row = next(data_iter)
+
+    for value, name in zip(first_row, COLUMNS):
+        alignment = ALIGNMENTS.get(type(value), 'l')
+        x.align[name] = alignment
+
+    # Now iterate over the data and add the rows.
+    x.add_row(first_row)
+    for row in data_iter:
+        row = [r.replace('\r\n', '\n').replace('\r', ' ')
+               if isinstance(r, six.string_types) else r
+               for r in row]
+        x.add_row(row)
+
     formatted = x.get_string(fields=COLUMNS)
     print(formatted)
     print('\n')
