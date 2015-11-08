8219689d3d3e2136301dc084d8c3d711f5471072
shad <kenny.shad7@gmail.com>
2015-11-07 23:46:41 -0500

    [TASK] General cleanup
    Updates based on feedback from Landscape.

--- tvrenamer/__init__.py
+++ tvrenamer/__init__.py
@@ -3,4 +3,4 @@ import pbr.version
 PROJECT_NAME = __package__
 __version__ = pbr.version.VersionInfo(PROJECT_NAME).version_string()
 
-__all__ = [__version__, PROJECT_NAME]
+__all__ = ['__version__', 'PROJECT_NAME']
